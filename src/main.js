3b3a442eb48ee60e15bf48187e171602bda3b727
qq584037793 <65120534+qq584037793@users.noreply.github.com>
2021-05-21 14:22:30 +0900

    lazyLoad

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import lazyPlugin from 'vue3-lazy'
 
 // グローバルスタイルファイルを導入する
 import '@/assets/scss/index.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(lazyPlugin, {
+  loading: require('@/assets/images/default.png')
+}).mount('#app')
