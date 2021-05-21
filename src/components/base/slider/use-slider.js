2bd9e01fe81f9c974a5d75fc4641d65e57901b8e
qq584037793 <65120534+qq584037793@users.noreply.github.com>
2021-05-21 12:53:30 +0900

    slider

---
+++ src/components/base/slider/use-slider.js
@@ -0,0 +1,36 @@
+import BScroll from '@better-scroll/core'
+import Slide from '@better-scroll/slide'
+
+import { onMounted, onUnmounted, ref } from 'vue'
+
+BScroll.use(Slide)
+
+export default function useSlider (wrapperRef) {
+  const slider = ref(null)
+  const currentPageIndex = ref(0)
+
+  onMounted(() => {
+    const sliderVal = slider.value = new BScroll(wrapperRef.value, {
+      // slider基本の配置
+      click: true,
+      scrollX: true,
+      scrollY: false,
+      momentum: false,
+      bounce: false,
+      probeType: 2,
+      slide: true
+    })
+
+    sliderVal.on('slideWillChange', (page) => {
+      currentPageIndex.value = page.pageX
+    })
+  })
+  // マウントされた後,インスタンス破壊
+  onUnmounted(() => {
+    slider.value.destroy()
+  })
+
+  return {
+    slider, currentPageIndex
+  }
+}
