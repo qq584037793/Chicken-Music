3256b17940d95c63847fd97cbfeb73207a305c4e
qq584037793 <65120534+qq584037793@users.noreply.github.com>
2021-05-21 12:52:06 +0900

    slider

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,16 +1,52 @@
 <template>
-  <div class="recommend">おすすめ</div>
+  <div class="recommend">
+    <div class="slider-wrapper">
+      <div class="slider-content">
+        <slider v-if="sliders.length" :sliders="sliders"></slider>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import { getRecommend } from '@/service/recommend'
+import Slider from '@/components/base/slider/slider'
 
 export default {
   name: 'recommend',
+  components: {
+    Slider
+  },
+  data () {
+    return {
+      sliders: []
+    }
+  },
   async created () {
     const result = await getRecommend()
-    console.log(result)
+    this.sliders = result.sliders
   }
 }
 </script>
 <style lang="scss" scoped>
+.recommend {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  overflow: scroll;
+  .slider-wrapper {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-top: 40%;
+    overflow: hidden;
+    .slider-content {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
