18da692beeefd34db52295c74ff2b6dba6bc74fc
qq584037793 <65120534+qq584037793@users.noreply.github.com>
2021-05-22 14:17:42 +0900

    singerData

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -2,8 +2,14 @@
   <div class="singer">歌手</div>
 </template>
 <script>
+import { getSingerList } from '@/service/singer'
 export default {
-  name: 'singer'
+  name: 'singer',
+
+  async created () {
+    const result = await getSingerList()
+    console.log(result)
+  }
 }
 </script>
 <style lang="scss" scoped>
