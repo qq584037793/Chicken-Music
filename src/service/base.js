83bb7d463c7cf48f47dd89b6d03bb17dfe880ad8
qq584037793 <65120534+qq584037793@users.noreply.github.com>
2021-06-01 01:10:06 +0900

    fixed

--- src/service/base.js
+++ src/service/base.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const ERR_OK = 0
-const baseURL = '/'
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://106.14.157.145/music/' : '/'
 
 axios.defaults.baseURL = baseURL
 
